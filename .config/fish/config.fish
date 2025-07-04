f4711c8e5050a4c7f14368327b655924de88b1e1
nixuris <nixuriss@proton.me>
2025-07-04 15:54:01 +0700

    Aliases for nvim and nvidia launch arg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,9 @@ if status is-interactive
     alias xr="paru -Rns"
     alias xu="paru"
 
-
+    alias gpu="__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia"
     alias e="nvim"
+    alias vim="nvim"
+    alias vi="nvim"
     alias se="sudo -E -s nvim"
 end
